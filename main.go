7d0078cd5991ae933917653ed6e86340865af44b
Sean McGivern <sean@mcgivern.me.uk>
2022-06-26 20:07:12 +0100

    Query database (with limit) and display table of results

--- main.go
+++ main.go
@@ -3,28 +3,81 @@ package main
 import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
+	"html/template"
+	"net/http"
+	"os"
+	"log"
 	_ "modernc.org/sqlite"
 )
 
-func main() {
-	db := sqlx.MustConnect("sqlite", ":memory:")
-	defer db.Close()
+var db *sqlx.DB
 
-	rows, err := db.Queryx("SELECT 'a' AS first, 'b' AS second, 1 AS third")
+type Result struct {
+	Columns []string
+	Rows [][]interface{}
+	Messages []string
+}
+
+func runQuery(query string, limit int) Result {
+	messages := make([]string, 0)
+	rows := make([][]interface{}, 0)
+	i := 1
+
+	results, err := db.Queryx(query)
 	if err != nil {
-		fmt.Println(err)
+		return Result{ Messages: []string{err.Error()} }
 	}
-	columns, err := rows.Columns()
+
+	columns, err := results.Columns()
 	if err != nil {
-		fmt.Println(err)
+		messages = append(messages, err.Error())
 	}
-	fmt.Println(columns)
-	for rows.Next() {
-		// cols is an []interface{} of all of the column results
-		cols, err := rows.SliceScan()
-		if err != nil {
-			fmt.Println(err)
+
+	for results.Next() {
+		if i > limit {
+			messages = append(messages, fmt.Sprintf("more rows than limit (%d)", limit))
+			break
 		}
-		fmt.Println(cols)
+
+		cols, err := results.SliceScan()
+		if err != nil {
+			messages = append(messages, err.Error())
+		}
+
+		rows = append(rows, cols)
+		i += 1
 	}
+
+	return Result {
+		Columns: columns,
+			Rows: rows,
+			Messages: messages,
+	}
+}
+
+func table(w http.ResponseWriter, r *http.Request) {
+	template := template.Must(template.New("table.html").ParseFiles("template/table.html"))
+
+	template.Execute(w, runQuery("SELECT * FROM men_test_batting_innings LIMIT 10;", 9))
+}
+
+func logRequests(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	db = sqlx.MustConnect("sqlite", "data/innings.sqlite3")
+
+	port, exists := os.LookupEnv("PORT")
+
+	if !exists {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", table)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), logRequests(http.DefaultServeMux)))
 }
